484183c9de0fa2d0f1ee389fc462666da6722b3b
Vivek Singh <petmongrels@gmail.com>
2018-06-07 17:50:10 +0530

    all mandatory fields completed

--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
-deploy:
-	ssh fmes "rm -f /var/www/html/ijmewp/paymentapp/*.php"
-	ssh fmes "rm -f /var/www/html/ijmewp/paymentapp/*.html"
-	ssh fmes "rm -f /var/www/html/ijmewp/paymentapp/*.js"
-	scp *.php *.html *.js fmes:/var/www/html/ijmewp/paymentapp/
+define _deploy
+	ssh fmes "rm -f /var/www/html/ijmewp/paymentapp/*.$1"
+	scp *.$1 fmes:/var/www/html/ijmewp/paymentapp/
+endef
+
+deploy_php:
+	$(call _deploy,php)
+
+deploy_html:
+	$(call _deploy,html)
+
+deploy_js:
+	$(call _deploy,js)
+
+deploy: deploy_html deploy_js deploy_php
--- ccavResponseHandler.php
+++ ccavResponseHandler.php
@@ -1,9 +1,9 @@
 <?php include('Crypto.php')?>
+<?php include('accessDetails.php') ?>
 <?php
+	error_reporting(-1);
 
-	error_reporting(0);
-	
-	$workingKey='';		//Working Key should be provided here.
+	$workingKey=CCAVENUE_WORKING_KEY;		//Working Key should be provided here.
 	$encResponse=$_POST["encResp"];			//This is the response sent by the CCAvenue Server
 	$rcvdString=decrypt($encResponse,$workingKey);		//Crypto Decryption used as per the specified working key.
 	$order_status="";
@@ -11,21 +11,22 @@
 	$dataSize=sizeof($decryptValues);
 	echo "<center>";
 
-	for($i = 0; $i < $dataSize; $i++) 
+	for($i = 0; $i < $dataSize; $i++)
 	{
 		$information=explode('=',$decryptValues[$i]);
-		if($i==3)	$order_status=$information[1];
+		if($i == 3)
+		    $order_status=$information[1];
 	}
 
 	if($order_status==="Success")
 	{
 		echo "<br>Thank you for shopping with us. Your credit card has been charged and your transaction is successful. We will be shipping your order to you soon.";
-		
+
 	}
 	else if($order_status==="Aborted")
 	{
 		echo "<br>Thank you for shopping with us.We will keep you posted regarding the status of your order through e-mail";
-	
+
 	}
 	else if($order_status==="Failure")
 	{
@@ -34,13 +35,13 @@
 	else
 	{
 		echo "<br>Security Error. Illegal access detected";
-	
+
 	}
 
 	echo "<br><br>";
 
 	echo "<table cellspacing=4 cellpadding=4>";
-	for($i = 0; $i < $dataSize; $i++) 
+	for($i = 0; $i < $dataSize; $i++)
 	{
 		$information=explode('=',$decryptValues[$i]);
 	    	echo '<tr><td>'.$information[0].'</td><td>'.$information[1].'</td></tr>';
