0f97533abf098ccb6c9e01f3977f3632bea9732b
Vivek Singh <petmongrels@gmail.com>
2018-06-13 14:39:27 +0530

    displaying more friendly response

--- ccavResponseHandler.php
+++ ccavResponseHandler.php
@@ -20,22 +20,20 @@
 
 	if($order_status==="Success")
 	{
-		echo "<br>Thank you for shopping with us. Your credit card has been charged and your transaction is successful. We will be shipping your order to you soon.";
+		echo "<br>Thank you for paying for the registration. Your credit/debit card has been charged and your transaction is successful. We will be shipping your order to you soon.";
 
 	}
 	else if($order_status==="Aborted")
 	{
-		echo "<br>Thank you for shopping with us.We will keep you posted regarding the status of your order through e-mail";
-
+		echo "<br>Thank you for paying for the registration. We will keep you posted regarding the status of your order through e-mail";
 	}
 	else if($order_status==="Failure")
 	{
-		echo "<br>Thank you for shopping with us.However,the transaction has been declined.";
+		echo "<br>Thank you for your payment request. However,the transaction has been declined.";
 	}
 	else
 	{
 		echo "<br>Security Error. Illegal access detected";
-
 	}
 
 	echo "<br><br>";
